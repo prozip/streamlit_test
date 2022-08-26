0d3ec83d0c9b617cc964bc4f332f6d38a2fb9807
prozip <quachkimson2000@gmail.com>
2022-08-26 21:36:27 +0700

    test pipe

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
 import streamlit as st
 
+
+import torch
+from diffusers import StableDiffusionPipeline
+
+# make sure you're logged in with `huggingface-cli login`
+pipe = StableDiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4", 
+        revision="fp16", torch_dtype=torch.float16, use_auth_token="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")  
+
+
 st.title("AI Art Tool")
 st.write("Text to Image generator")
 
 promp = st.text_input("Input promp:")
 st.write(promp)
+st.write(pipe)
+
+
+
